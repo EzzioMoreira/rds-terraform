267e7b2bfbd00762fae154ebf053008212ab41dc
EzzioMoreira <enzziom@gmail.com>
2022-08-21 22:56:43 -0300

    test: format output infraops

--- variable.tf
+++ variable.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 #### The variables for RDS
 variable "storage" {
   type        = number
-  default     = 10
+  default     = 30
   description = "The allocated storage"
 }
 
