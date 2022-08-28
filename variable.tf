c1b06e60cd71c081615c4a0259d6e5976fe18422
EzzioWMoreira <ezzio.moreira@pagar.me>
2022-08-28 13:51:21 -0300

    fest: secret

--- variable.tf
+++ variable.tf
@@ -30,7 +30,7 @@ variable "engine_version" {
 }
 
 variable "instance_type" {
-  default     = "db.t2.small"
+  default     = "db.t2.nano"
   type        = string
   description = "The RDS instance class"
 }
